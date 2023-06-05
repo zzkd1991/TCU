1c69e1c4850b132b9a7d0fd681f564260d12cde8
unknown <zhangzhen>
2023-06-05 20:45:10 +0800

    添加DO+，DO-功能

--- Src/bsp_output_do.h
+++ Src/bsp_output_do.h
@@ -19,6 +19,8 @@ uint8_t API_OUT_Do_Cfg(uint8_t channel_u8, uint8_t OutMode_u8);
 
 uint8_t API_OUT_Do_Set(uint8_t channel_u8, uint8_t value);
 
+uint8_t API_OUT_Do_Get_State(uint8_t channel_u8);
+
 #endif
 
 

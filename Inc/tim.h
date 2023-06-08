15bd609c6cebc245b1aa8067f47a187fdd7e5a6b
Huanz_789 <1715530178@qq.com>
2023-06-08 10:33:41 +0800

    提交PI部分，修改文件设计tim.c和stm32f4xx_it.c、bsp_main.c 以及头文件

--- Inc/tim.h
+++ Inc/tim.h
@@ -24,18 +24,53 @@
 extern "C" {
 #endif
 
-#include <stdint.h>	
-	
-#define TLE7242_TIM1	TIM1
-#define TLE7242_TIM1_CLK_ENABLE()		__TIM1_CLK_ENABLE()
+#include "main.h"
 
-#define TLE7242_CLK_PIN							GPIO_PIN_8
-#define TLE7242_GPIO_PORT						GPIOA
-#define TLE7242_GPIO_CLK_ENABLE()		__GPIOA_CLK_ENABLE()
-#define TLE7242_CLK_AF							GPIO_AF1_TIM1
-	
-void TIMx1_GPIO_Config(void);
-void TIM1_Mode_Config(uint32_t pwm_freq);
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern TIM_HandleTypeDef htim1;
+
+extern TIM_HandleTypeDef htim2;
+
+extern TIM_HandleTypeDef htim3;
+
+extern TIM_HandleTypeDef htim4;
+
+extern TIM_HandleTypeDef htim5;
+
+extern TIM_HandleTypeDef htim6;
+
+extern TIM_HandleTypeDef htim8;
+
+extern TIM_HandleTypeDef htim9;
+
+extern TIM_HandleTypeDef htim12;
+
+/* USER CODE BEGIN Private defines */
+#define abs(x)    ((x)<=0?(-x):(x))
+extern uint16_t Frequency[6];
+extern uint32_t PI_Single_Pulse_Count[6];
+extern uint8_t OverflowCount[2];
+extern uint32_t PI_Double_Pulse_Count[2];
+/* USER CODE END Private defines */
+
+void MX_TIM1_Init(void);
+void MX_TIM2_Init(void);
+void MX_TIM3_Init(void);
+void MX_TIM4_Init(void);
+void MX_TIM5_Init(void);
+void MX_TIM6_Init(void);
+void MX_TIM8_Init(void);
+void MX_TIM9_Init(void);
+void MX_TIM12_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+uint16_t API_PI_Double_Dir_Get(uint8_t group_u8);
+void API_PI_Double_Count_Reset(uint8_t group_u8);
+/* USER CODE END Prototypes */
+
 	
 
 #ifdef __cplusplus
--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -63,6 +63,11 @@
 extern DMA_HandleTypeDef hdma_adc1;
 extern ADC_HandleTypeDef hadc1;
 #endif
+
+extern DAC_HandleTypeDef hdac;
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim6;
+extern TIM_HandleTypeDef htim8;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -430,7 +435,75 @@ void DMA2_Stream1_IRQHandler(void)
   /* USER CODE END DMA2_Stream1_IRQn 1 */
 }
 
+/**
+  * @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
+  */
+void TIM1_UP_TIM10_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
 
+  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
+
+  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM1 capture compare interrupt.
+  */
+void TIM1_CC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_CC_IRQn 0 */
+
+  /* USER CODE END TIM1_CC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_CC_IRQn 1 */
+
+  /* USER CODE END TIM1_CC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM8 update interrupt and TIM13 global interrupt.
+  */
+void TIM8_UP_TIM13_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM8_UP_TIM13_IRQn 0 */
+
+  /* USER CODE END TIM8_UP_TIM13_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim8);
+  /* USER CODE BEGIN TIM8_UP_TIM13_IRQn 1 */
+
+  /* USER CODE END TIM8_UP_TIM13_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM8 capture compare interrupt.
+  */
+void TIM8_CC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM8_CC_IRQn 0 */
+
+  /* USER CODE END TIM8_CC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim8);
+  /* USER CODE BEGIN TIM8_CC_IRQn 1 */
+
+  /* USER CODE END TIM8_CC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM6 global interrupt, DAC1 and DAC2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim6);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
+}
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
